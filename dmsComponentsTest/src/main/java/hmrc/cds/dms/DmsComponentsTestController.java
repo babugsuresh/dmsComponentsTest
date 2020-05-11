a93b119b8a36b3cb901c74145a9fa61e23cffb4d
Suresh Babu G <babugsuresh@users.noreply.github.com>
2020-05-11 19:06:27 +0100

    final commit

--- dmsComponentsTest/src/main/java/hmrc/cds/dms/DmsComponentsTestController.java
+++ dmsComponentsTest/src/main/java/hmrc/cds/dms/DmsComponentsTestController.java
@@ -6,18 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
-import java.io.StringWriter;
-import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -52,7 +48,7 @@ import com.ibm.wsdl.PortTypeImpl;
 @Controller
 public class DmsComponentsTestController {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	private static final String BASEDIR = null;
 
 	@GetMapping("/test")
@@ -76,79 +72,67 @@ public class DmsComponentsTestController {
 
 		@SuppressWarnings("unchecked")
 		List<String> envs = (List<String>) yamlMaps.get("Environments");
+
 		@SuppressWarnings("unchecked")
 		final List<Map<String, Object>> systemMapping = (List<Map<String, Object>>) yamlMaps.get("SystemMapping");
+
+		List<OperationName> operationNames = new ArrayList<OperationName>();
+		List<ServiceName> serviceNames = new ArrayList<ServiceName>();
+		List<ReportBean> rb = new ArrayList<ReportBean>();
+
+		// Tempt list to ensure wsdls are not called again
+		List<String> list = new ArrayList<String>();
+
+		String endpoint = null;
+		String systemtocall = null;
+		boolean loopBack = false;
+		boolean oneenv = false;
+		int index = 0;
 
 		File directory = new File(BASEDIR, "src/main/resources/wsdls/99 Assembled adapters 3.2.8.11");
 		File wsdlDir = null;
 		File file;
-		String[] wsdlNames;
 
-		//ReportBean reportBean = new ReportBean();
+		// Setting only one env selected
+		if (envs.contains(envSelected)) {
+			log.info("Env Selected: " + envSelected + ", Index for Selected: " + envs.indexOf(envSelected));
+			oneenv = true;
+		}
 
-		List<OperationName> operationNames = new ArrayList<OperationName>();
-		List<OperationName> rempOperationNames = new ArrayList<OperationName>();
-		List<ServiceName> serviceNames = new ArrayList<ServiceName>();
-		List<ReportBean> rb = new ArrayList<ReportBean>();
+		exitloop: for (String env : envs) {
 
-		List<String> list = new ArrayList<String>();
-
-		String endpoint = null;
-		String soapenvelope = null;
-		String systemtocall = null;
-		boolean loopBack = false;
-
-		//Iterator<File> iter = FileUtils.iterateFiles(directory, new String[] { "wsdl" }, true);
-
-		for (String env : envs) {
-			System.out.println("Test env: " + env);
-			Iterator<File> iter = FileUtils.iterateFiles(directory, new String[] { "wsdl" }, true);
+			// Setting selected env and its index for rite end point call purpose
+			if (oneenv) {
+				index = envs.indexOf(envSelected);
+				env = envSelected;
+			}
 
 			ReportBean reportBean = new ReportBean();
 
+			// when it comes back for new env clear this
 			if (!list.isEmpty()) {
 				list.removeAll(list);
-
 			}
 
+			// iterate at nested dir level
+			Iterator<File> iter = FileUtils.iterateFiles(directory, new String[] { "wsdl" }, true);
 			while (iter.hasNext()) {
 
 				file = (File) iter.next();
-				// System.out.println("\nLOOP2: " + ii++ + ", path: " + file.getParent());
-				// System.out.println("\nWSDL Found: " + file.getName());
-				// System.out.println("file Path: " + file.getPath());
-				// System.out.println("file dir: " + file.getParent());
 				wsdlDir = new File(BASEDIR, file.getParent());
 
+				// iterate at wsdl dir level and look for wsdl only and no more traverse through
+				// wsdl dir
 				Iterator<File> itr = FileUtils.iterateFiles(wsdlDir, new String[] { "wsdl" }, false);
 				while (itr.hasNext()) {
 					file = (File) itr.next();
-
-					/*
-					 * System.out.println("\nWSDL Found: " + file.getName());
-					 * System.out.println("file Path: " + file.getPath());
-					 * System.out.println("file dir: " + file.getParent());
-					 */
-
 					File wsdlFile = new File(wsdlDir, file.getPath());
-
-					/*
-					 * if(!operationNames.isEmpty()) {
-					 * 
-					 * rempOperationNames.addAll(operationNames);
-					 * operationNames.removeAll(operationNames); }
-					 */
-
 					if (!list.contains(wsdlFile.getName())) {
-						// System.out.println("wsdlFile: "+wsdlFile.toURI());
 
 						outerloop: for (Map<String, Object> featureService : systemMapping) {
 							for (Map.Entry<String, Object> entry : featureService.entrySet()) {
-								// System.out.println("Key: " + entry.getKey()+", wsdlName:
-								// "+wsdlFile.getName());
 								String key = entry.getKey();
 								if (wsdlFile.getName().equalsIgnoreCase(key)) {
-									System.out.println("Key: " + entry.getKey() + ", wsdlName: " + wsdlFile.getName());
 									String value = (String) entry.getValue();
 									systemtocall = value;
 									list.add(wsdlFile.getName());
@@ -161,55 +145,43 @@ public class DmsComponentsTestController {
 							}
 						}
 
-						System.out.println("Eissssss: " + list + ", \n" + list.contains(wsdlFile.getName()));
-
 						if (!loopBack) {
-							System.out.println("Env Executing for: " + env + "\nSystem to call for "
-									+ wsdlFile.getName() + "\nWSDL Service is: " + systemtocall
-									+ "\nindex of environment: " + envs.indexOf(env));
+							log.info("Env Executing for: " + env + ", System to call for: " + wsdlFile.getName()
+									+ ", WSDL Service is: " + systemtocall + ", index of environment: "
+									+ envs.indexOf(env));
 
-							endpoint = getEndPoint(env, envs.indexOf(env), systemtocall, yamlMaps);
+							// one env selected scenario
+							if (oneenv) {
+								endpoint = getEndPoint(env, index, systemtocall, yamlMaps);
+							} else {
+								endpoint = getEndPoint(env, envs.indexOf(env), systemtocall, yamlMaps);
+							}
+
 							ServiceName services = new ServiceName();
 							if (!(endpoint == null)) {
 								String systemHostURI = systemtocall + "URI";
 								endpoint = endpoint + yamlMaps.get(systemHostURI);
-
-								System.out.println("Final Endpoint URL for: " + systemtocall + " = " + endpoint);
+								log.info("Endpoint URL to call for: " + systemtocall + " = " + endpoint);
 
 								List<Operation> operationList = getPortTypeOperations(file.getPath());
 
-								System.out.println("Total Number of operations in " + wsdlFile.getName() + " : "
+								log.info("Total Number of operations in " + wsdlFile.getName() + " : "
 										+ operationList.size());
 
-								// List<OperationName> operationNames = new ArrayList<OperationName>();
-
 								operationNames = getOperations(operationList, endpoint);
-								/*
-								 * services = new ServiceName(); services.setSystemName(systemtocall);
-								 * services.setServiceName(wsdlFile.getName());
-								 * services.setOperationNames(rempOperationNames);
-								 * 
-								 * serviceNames.add(services); rempOperationNames.removeAll(rempOperationNames);
-								 * System.out.println("serviceName Added successfully" + services);
-								 */
 
 							} else {
-								System.out.println("No External System End Points are Configured for: " + systemtocall);
+								log.info("No External System End Points are Configured for: " + systemtocall);
 							}
-
-							services = new ServiceName();
 
 							services = new ServiceName();
 							services.setSystemName(systemtocall);
 							services.setServiceName(wsdlFile.getName());
 							services.setOperationNames(operationNames);
-
 							serviceNames.add(services);
-							// rempOperationNames.removeAll(rempOperationNames);
-							System.out.println("serviceName Added successfully" + services);
 
 						} else {
-							System.out.println("No External System is configured for WSDL: " + wsdlFile.getName());
+							log.info("No External System is configured for WSDL: " + wsdlFile.getName());
 						}
 
 					}
@@ -218,45 +190,36 @@ public class DmsComponentsTestController {
 
 			}
 
+			// Just to avoid duplicate adding
 			List<ServiceName> tempServiceList = new ArrayList<ServiceName>();
-
 			tempServiceList.addAll(serviceNames);
 
 			reportBean = new ReportBean();
 			reportBean.setEnvName(env);
 			reportBean.setServiceNames(tempServiceList);
-			rb.add(reportBean);
+
+			// exit main env level loop if one env only selected
+			if (envSelected.equalsIgnoreCase("all")) {
+				rb.add(reportBean);
+			} else {
+				rb.add(reportBean);
+				break exitloop;
+			}
+
 			serviceNames.removeAll(serviceNames);
 
-			// System.out.println("ReportBean Added successfully" + reportBean);
-
 		}
+		log.info("Final ReportBean: " + rb);
 
-		System.out.println("temp list:" + list);
-		// System.out.println("ReportBean Added successfully" + rb);
-
-		System.out.println("LOGGGGGGGGGGGGGGG");
-		System.out.println("operationNames: " + operationNames);
-		System.out.println("serviceNames: " + serviceNames);
-		System.out.println("ReportBean: " + rb);
-
+		// Now form a HTML data based on final ReportBean
 		String body = generateHTMLfile(rb);
-		
-		//System.out.println(body);
 
+		// Pass this HTML data to Thymeleaf html page
 		model.addAttribute("body", body);
 		return "report";
 	}
 
 	private String generateHTMLfile(List<ReportBean> rb) throws IOException {
-
-		System.out.println("JESUS is MY LORD");
-
-		// File htmlTemplateFile = new File("src/main/resources/Report2.html");
-
-		// String htmlString = FileUtils.readFileToString(htmlTemplateFile, "UTF-8");
-
-		// String title = "New Page";
 
 		String body = "";
 
@@ -277,13 +240,9 @@ public class DmsComponentsTestController {
 		String tag6 = "</td><td>";
 		String green = "<span class=\"label success\">";
 		String red = "<span class=\"label danger\">";
-		String blue = "<span class=\"label label-info\">";
 		String tag7 = "</span>";
 
 		String tag8 = "<button type=\"button\" class=\"collapsible\">";
-		String tag9 = "</button>";
-		String tag10 = "<div class=\"content\">";
-		String tag11 = "</div>";
 
 		// To Make response button to be clickable
 		String js = "<script>\r\n" + "var coll = document.getElementsByClassName(\"collapsible\");\r\n" + "var i;\r\n"
@@ -293,12 +252,6 @@ public class DmsComponentsTestController {
 				+ "    if (content.style.display === \"block\") {\r\n" + "      content.style.display = \"none\";\r\n"
 				+ "    } else {\r\n" + "      content.style.display = \"block\";\r\n" + "    }\r\n" + "  });\r\n"
 				+ "}\r\n" + "</script>";
-
-		// <button type="button" class="collapsible">Response</button>
-
-		// tag8+Response+tage9
-
-		// tag8+op.getResponse()+tag9
 
 		StringBuilder builder = new StringBuilder();
 
@@ -316,10 +269,9 @@ public class DmsComponentsTestController {
 					} else {
 						color = red + op.getStatus() + tag7;
 					}
-					// String cc = "<div class=\"content\">"+op.getResponse()+"</div>";
 					String cc = "<div class=\"content\">Testing</div>";
-					dataToLoad = tag4 + sn.getSystemName() + tag6 + sname + tag6 + op.getOperationName() + tag6 + color
-							+ tag6 + tag8 + "Response" + "</button>" + cc + tag5;
+					dataToLoad = tag4 + "<b>" + sn.getSystemName() + "</b>" + tag6 + sname + tag6
+							+ op.getOperationName() + tag6 + color + tag6 + tag8 + "Response" + "</button>" + cc + tag5;
 					localBuilder.append(dataToLoad);
 				}
 			}
@@ -331,44 +283,32 @@ public class DmsComponentsTestController {
 
 		String finalDataToPublish = builder.toString();
 
-		body = finalDataToPublish+js;
-
-		// htmlString = htmlString.replace("$title", title);
-		// htmlString = htmlString.replace("$body", body);
-		// File newHtmlFile = new File("src/main/resources/new.html");
-		// FileUtils.writeStringToFile(newHtmlFile, htmlString, "UTF-8");
+		// Creating final body
+		body = finalDataToPublish + js;
 
 		return body;
 
 	}
 
-	// }
-
 	private String getEndPoint(String env, int index, String system, Map<String, Object> yamlMaps) {
 		String endpoint = null;
 
 		String systemHost = system + "Hosts";
-		// System.out.println("Called here" + yamlMaps.toString() + ", systemHost: " +
-		// systemHost);
 
 		@SuppressWarnings("unchecked")
 		List<String> systemHosts = (List<String>) yamlMaps.get(systemHost);
-		// System.out.println("index: " + index + ", " + systemHosts);
 
 		if (systemHosts == null || systemHosts.isEmpty()) {
 			return endpoint;
 		} else {
 			if (!(systemHosts == null) || !(systemHosts.isEmpty())) {
 				for (String host : systemHosts) {
-					// System.out.println("index: " + systemHosts.indexOf(host));
 					if (index == systemHosts.indexOf(host)) {
-						// System.out.println("Called here"+systemHosts.indexOf(host));
-						// System.out.println("hosts---: " + host);
 						endpoint = host;
 					}
 
 				}
-				System.out.println("No of Endpoints configured for system : " + system + "=" + systemHosts.size());
+				log.info("No of Endpoints configured for system : " + system + "=" + systemHosts.size());
 			}
 		}
 
@@ -431,32 +371,26 @@ public class DmsComponentsTestController {
 
 	public List<OperationName> getOperations(List<Operation> operationList, String endpoint)
 			throws UnsupportedOperationException, SOAPException, IOException {
-
-		// String endpoint = null;
 		String soapenvelope = null;
-		String systemtocall = null;
-		boolean loopBack = false;
 
 		SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
 		SOAPConnection soapConnection = soapConnectionFactory.createConnection();
 
 		OperationName operationName = new OperationName();
 		List<OperationName> operationNames = new ArrayList<OperationName>();
-		List<ServiceName> serviceNames = new ArrayList<ServiceName>();
 		int i = 0;
 		for (Operation opname : operationList) {
-			System.out.println("List of Operations: " + opname.getName());
-			// System.out.println("---Started Looking for Request XMLs---");
+			log.info("List of Operations: " + opname.getName());
 
 			soapenvelope = getRequestEnvelopeToString(opname.getName());
 
 			if (!soapenvelope.equalsIgnoreCase("empty")) {
 
-				System.out.println("Request XML available for Operation: " + opname.getName());
+				log.info("Request XML available for Operation: " + opname.getName());
 				System.setProperty("java.net.useSystemProxies", "true");
-				System.out.println("SOAP Request Payload: " + soapenvelope);
+				// log.info("SOAP Request Payload: " + soapenvelope);
 
-				System.out.println("SOAP endpoint to Call: " + endpoint);
+				log.info("SOAP endpoint to Call: " + endpoint);
 
 				InputStream is = new ByteArrayInputStream(soapenvelope.getBytes());
 				SOAPMessage request = MessageFactory.newInstance().createMessage(null, is);
@@ -467,21 +401,16 @@ public class DmsComponentsTestController {
 
 				SOAPMessage soapResponse = soapConnection.call(request, endpoint);
 
-				// System.out.println(printSOAPResponse(soapResponse));
-				// ByteArrayOutputStream streamOut = new ByteArrayOutputStream();
-
-				StringWriter sw = new StringWriter();
-
 				try {
 					TransformerFactory transformerFactory = TransformerFactory.newInstance();
 					Transformer transformer = transformerFactory.newTransformer();
 					Source sourceContent = soapResponse.getSOAPPart().getContent();
 
-					System.out.print("\nResponse SOAP Message = ");
-					System.out.print("\n");
-					// StreamResult result = new StreamResult(System.out);
-					StreamResult result = new StreamResult(sw);
+					log.info("\nResponse SOAP Message = ");
+					log.info("\n");
+					StreamResult result = new StreamResult(System.out);
 					transformer.transform(sourceContent, result);
+					log.debug("::"+sourceContent);
 
 				} catch (TransformerException e) {
 					throw new RuntimeException(e);
@@ -492,20 +421,20 @@ public class DmsComponentsTestController {
 
 				if (!soapResponse.getSOAPBody().hasFault()) {
 					operationName.setStatus("Running");
-				} else {
+				}
+				// Handle Connection timeout / Conn refused
+				else if (soapResponse.getSOAPBody() == null) {
 					operationName.setStatus("  Down ");
+					// Handle SOAPFault Exception
+				} else {
+					operationName.setStatus("Fault");
 				}
 				// operationName.setResponse(sw.toString());
 				operationName.setResponse("TEST" + i++);
 				operationNames.add(operationName);
-				System.out.println("operationName Added successfully" + operationNames);
-
-				// System.out.println("\noperationNames: " + operationNames);
-
-				// reportBean.setEnvName(env);
 
 			} else {
-				System.out.println("No Request XML is provided for: " + opname.getName());
+				log.info("No Request XML is provided for: " + opname.getName());
 			}
 
 		}
